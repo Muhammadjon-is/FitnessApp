3b8748c5136914160e25721763ec246ca6c11447
Mukhammadjon <93770723+Muhammadjon-is@users.noreply.github.com>
2023-10-12 15:32:36 +0200

    navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Navbar from "./Scenes/Navbar";
 
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
   return (
     <div className="app bg-gray-20">
   <h1>Hello</h1>
-    <Navbar/>
+    <Navbar 
+     selectedPage={selectedPage}
+     setSelectedPage={setSelectedPage}
+    />
     </div>
   );
 }
--- src/Scenes/Navbar/index.tsx
+++ src/Scenes/Navbar/index.tsx
@@ -1,13 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import {Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
-import Logo from "@/assets/Logo.png"
+import React, { useEffect, useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "../../assets/Logo.png";
+import Link from "./link";
 
-type Props ={}
-const Navbar = () => {
-    const felxBetween = "flex items-center justify-between"
+type Props = {
+  selectedPage:string
+  setSelectedPage:(value:string) => void;
+};
+const Navbar = ({selectedPage, setSelectedPage}: Props) => {
+
+
+  const felxBetween = "flex items-center justify-between";
+
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <div className={`${felxBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${felxBetween} mx-auto w-5/6`}>
+          <div className={`${felxBetween} w-full gap-16`}>
+            <img src={Logo} alt="Logo" />
+            {/* Left site */}
 
-export default Navbar
+            {/* Right site */}
+            <div className={`${felxBetween} w-full`}>
+              {/* Inner Lefit */}
+              <div className={`${felxBetween} gap-8 text-sm`}>
+                <Link
+                 page="Home" 
+                 selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+                 />
+                <Link 
+                page="Benefits"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+                />
+                <Link 
+                page="Our classes"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+                />
+                <Link 
+                page="Contact us"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+                />
+              </div>
+              {/* INner right  */}
+              <div className={`${felxBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a Member</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
