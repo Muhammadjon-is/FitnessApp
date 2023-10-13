3c8b9f2f60818278735fd73c32c60f43e8770825
Mukhammadjon <93770723+Muhammadjon-is@users.noreply.github.com>
2023-10-13 11:35:59 +0200

    emmits are done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
-import Navbar from "./Scenes/Navbar";
+import { useEffect, useState } from "react";
+import Navbar from "./Scenes/Navbar/index";
+import { SelectedPage } from "./Scenes/shared/types";
+
 
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState('home');
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
     <div className="app bg-gray-20">
-  <h1>Hello</h1>
+
     <Navbar 
+     isTopOfPage={isTopOfPage}
      selectedPage={selectedPage}
      setSelectedPage={setSelectedPage}
     />
---
+++ src/Scenes/shared/types.ts
@@ -0,0 +1,6 @@
+export enum  SelectedPage {
+    Home = "home",
+    Benefits = "benefits",
+    Ourclasses = "ourClasses",
+    ContactUs = "contactus",
+  }
